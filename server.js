b3f62e690b87e77979e113811c3a4aecee066a7d
andrew karpisz <andrew.karpisz@gmail.com>
2020-09-20 19:38:46 -0600

    basic layout, packages, and backend setup

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 4269;
+const db = require("./models/");
+
+
+
+
+
+db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+  });
